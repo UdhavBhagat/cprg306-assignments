bcf7bd7163d9fcfc405ef3f79fa6f260f09fb0d5
UdhavBhagat <Udhav.Bhagat@edu.sait.ca>
2024-10-01 20:41:55 -0600

    Week 3 complete

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ export default function Page() {
   return(
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a href="./week-2">Week - 2 Assignment\</a>
+      <a href="./week-2">Week - 2 Assignment</a> <br />
+      <a href="./week-3">Week - 3 Assignment</a>
     </main>
   )
 }
---
+++ app/week-3/item.js
@@ -0,0 +1,8 @@
+export default function Item(props) {
+    return(
+        <li className="list-none bg-slate-900 m-4 max-w-sm">
+            <h1 className="text-xl font-bold">{props.name}</h1>
+            <p>Buy {props.quantity} in {props.category}</p>
+        </li>
+    )
+}
