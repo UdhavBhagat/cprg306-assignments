1b2c392dd355d1468cc6c3a44807c118bc20a409
UdhavBhagat <Udhav.Bhagat@edu.sait.ca>
2024-10-22 23:05:37 -0600

    Week6 Assignment

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Page() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <a href="./week-2">Week - 2 Assignment</a> <br />
       <a href="./week-3">Week - 3 Assignment</a><br />
-      <a href="./week-4">Week - 4 Assignment</a>
+      <a href="./week-4">Week - 4 Assignment</a><br />
+      <a href="./week-6">Week - 6 Assignment</a>
     </main>
   )
 }
