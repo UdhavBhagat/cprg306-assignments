e85e0534cb510a9890ed38765e394fbf2f2a029b
UdhavBhagat <Udhav.Bhagat@edu.sait.ca>
2024-11-19 22:09:55 -0700

    Added link to pages

--- app/page.js
+++ app/page.js
@@ -6,7 +6,10 @@ export default function Page() {
       <a href="./week-3">Week - 3 Assignment</a><br />
       <a href="./week-4">Week - 4 Assignment</a><br />
       <a href="./week-6">Week - 6 Assignment</a><br />
-      <a href="./week-7">Week - 7 Assignment</a>
+      <a href="./week-7">Week - 7 Assignment</a><br />
+      <a href="./week-8">Week - 8 Assignment</a><br />
+      <a href="./week=9">Week - 9 Assignment</a><br />
+      <a href="./week-10">Week - 10 Assignment</a>
     </main>
   )
 }
